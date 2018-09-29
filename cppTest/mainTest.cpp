fd2583f695cd2363a3f672646d5069fffd0dd0c4
Synteresis <synteresis@outlook.com>
2018-09-29 11:22:44 -0500

    Repeated Words

--- cppTest/mainTest.cpp
+++ cppTest/mainTest.cpp
@@ -2,55 +2,11 @@
 
 int main()
 {
-	/*
-	cout << "Please enter your first name (followed by 'enter'):\n";
-	string first_name; // first_name is a variable of type string
-	cin >> first_name; // read characters into first_name
-	cout << "Hello, " << first_name << "!\n";
-
-	int x = 1;
-	int h = 2;
-	bool y = true;
-	double z = 1.11111111;
-	float i = 1.111f;
-	char t = 't';
-	string g = "g";
-
-	cout << x << endl;
-	cout << h  << endl;
-	x << h;
-	cout << x << endl;
-
-	cout << "Please enter a floating-point value: ";
-	double n;
-	cin >> n;
-	cout << "n == " << n
-		<< "\nn+1 == " << n + 1
-		<< "\nthree times n == " << 3 * n
-		<< "\ntwice n == " << n + n
-		<< "\nn squared == " << n * n
-		<< "\nhalf of n == " << n / 2
-		<< "\nsquare root of n == " << sqrt(n)
-		<< '\n'; // another name for newline (“end of line”) in output
-	
-	cout << "\a";
-	string x = "Bobby";
-	cout << x << endl;
-	string z = "Bill" + x;
-	cout << x << endl;
-	cout << z << endl;
-	x += "samurai";
-	cout << x << endl;
-	cout << z << endl;
-	cout << "\a";
-	system("pause");
-	cout << "\a";
-	system("pause");
-	cout << "\a";
-	system("pause");
-	cout << "\a";
-	*/
-
-	system("pause");
-	return 0;
+	string previous = " "; // previous word; initialized to “not a word”
+	string current; // current word
+	while (cin >> current) { // read a stream of words
+		if (previous == current) // check if the word is the same as last
+			cout << "repeated word: " << current << '\n';
+		previous = current;
+	}
 }
