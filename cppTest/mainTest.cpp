ae7f063c607730903a583bbcfcc06b1a6def97e1
Synteresis <synteresis@outlook.com>
2018-09-29 21:27:32 -0500

    Program Lock

--- cppTest/mainTest.cpp
+++ cppTest/mainTest.cpp
@@ -4,46 +4,32 @@
 
 int main()
 {
-	vector<int> fVector = {1,2,3,4,5,6,7,8,9,10};
-	for(int i = 0; i < fVector.size(); ++i)
+	int i = 1;
+	constexpr int x = 1000;
+	MSG msg = { 0 };
+	for (int z = 101; z != 0; ++z)
 	{
-		cout << fVector[i] << endl;
+		if (i = (1000 + (z - 100) - x))
+		{
+			RegisterHotKey(NULL, 1, MOD_NOREPEAT, VK_F9);
+			cout << i << endl;
+		}
+		if (GetMessage(&msg, NULL, 0, 0) != 0)
+		{
+			UnregisterHotKey(NULL, 1);
+			if (i != (1000 + (z - 100) - x))
+			{
+				RegisterHotKey(NULL, 2, MOD_NOREPEAT, VK_F10);
+				do
+				{
+					cout << "Error: Value Modified. Press F10 To Unmodify." << endl;
+					if (GetMessage(&msg, NULL, 0, 0) != 0)
+						i = (1000 + (z - 100) - x); UnregisterHotKey(NULL, 2); break;
+				} while (true);
+			}
+			++i; 
+		}
 	}
 
-	vector<string> sVector = { "Bill","Nye","The","Science","Guy" };
-	for (int i = 0; i < sVector.size(); ++i)
-	{
-		cout << sVector[i] << endl;
-	}
-
-	vector<int> tVector(3);
-	for (int i = 0; i < 3; ++i)
-	{
-		tVector[i] = i;
-		cout << tVector[i] << endl;
-	}
-
-	cout << endl;
-
-	vector<int> v = { 5, 7, 9, 4, 6, 8 };
-	for (int x : v) // for each x in v
-		cout << x << '\n';
-
-	cout << endl;
-
-	int z[] = { 1, 2, 3, 4, 5, 6, 7 };
-	for (int x : z)
-		cout << z << endl;
-
-	/*
-	vector<int> f2Vector;
-	for (int i = 0; i < 100; ++i)
-	{
-		f2Vector[i] = i;
-		cout << f2Vector[i] << endl;
-	}
-	*/
-
-	system("pause");
 	return 0;
 }
